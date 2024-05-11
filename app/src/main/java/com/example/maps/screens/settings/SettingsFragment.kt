747e2db1d77793e654ea4495d94b3ce8e7126e8e
tanya <swishtan@mail.ru>
2024-05-11 12:02:49 +0300

    TASK-15: Добавила отображение статистики,экрана настроек и изменила дизайн

--- app/src/main/java/com/example/maps/screens/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/maps/screens/settings/SettingsFragment.kt
@@ -1,10 +1,61 @@
 package com.example.maps.screens.settings
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.maps.R
+import com.example.maps.databinding.FragmentSettingsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    private val vm: SettingsVM by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+        setToolbar()
+        bindUI()
+        observeVM()
+
+        return binding.root
+    }
+
+    private fun bindUI() = with(binding) {
+        etName.setText(vm.getName())
+        etWeight.setText(vm.getWeight())
+        btnApplyChanges.setOnClickListener {
+            applyChanges()
+        }
+    }
+
+    private fun observeVM() {
+        vm.error.observe(viewLifecycleOwner) { error ->
+            binding.titleNameText.error = error
+        }
+    }
+
+    private fun applyChanges() {
+        val success = vm.applyChanges(
+            name = binding.etName.text.toString(),
+            weight = binding.etWeight.text.toString()
+        )
+        if (success) setToolbar()
+    }
+
+    private fun setToolbar() {
+        binding.tvTitle.text = "LET'S GO, ${vm.getName().toUpperCase()}"
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
 }
--- app/src/main/java/com/example/maps/screens/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/example/maps/screens/statistics/StatisticsFragment.kt
@@ -1,14 +1,23 @@
 package com.example.maps.screens.statistics
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.maps.R
 import com.example.maps.databinding.FragmentStatisticsBinding
+import com.example.maps.other.CustomMarkerView
+import com.example.maps.other.TrackingUtility
+import com.github.mikephil.charting.components.XAxis.XAxisPosition.BOTTOM
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.round
 
 @AndroidEntryPoint
 class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
@@ -19,7 +28,86 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentStatisticsBinding.inflate(inflater, container, false)
-        return (binding.root)
+
+        observeVM()
+        setToolbar()
+        setupBarChart()
+        vm.getSortedByDate()
+        vm.getTotalTimeRun()
+        vm.getTotalDistance()
+        vm.getTotalCaloriesBurned()
+        vm.getTotalAvgSpeed()
+
+        return binding.root
+    }
+
+    private fun observeVM() {
+        vm.statisticsLD.observe(viewLifecycleOwner) {
+            it?.let {
+                val allAvgSpeeds = it.indices.map { i -> BarEntry(i.toFloat(), it[i].avgSpeedInKMH) }
+                val barDataSet = BarDataSet(allAvgSpeeds, "Avg Speed Over Time").apply {
+                    valueTextColor = Color.WHITE
+                    color = ContextCompat.getColor(requireContext(), pub.devrel.easypermissions.R.color.tooltip_background_dark)
+                }
+                binding.barChart.data = BarData(barDataSet)
+                binding.barChart.marker = CustomMarkerView(it.reversed(), requireContext(), R.layout.marker_view)
+                binding.barChart.invalidate()
+            }
+        }
+        vm.timeRunLD.observe(viewLifecycleOwner) {
+            it?.let {
+                binding.tvTotalTime.text = TrackingUtility.getFormattedStopWatchTime(it)
+            }
+        }
+        vm.distanceLD.observe(viewLifecycleOwner) {
+            it?.let {
+                val km = it / 1000f
+                val totalDistance = round(km * 10f) / 10f
+                val totalDistanceString = "${totalDistance}km"
+                binding.tvTotalDistance.text = totalDistanceString
+            }
+        }
+        vm.caloriesBurnedLD.observe(viewLifecycleOwner) {
+            it?.let {
+                val totalCalories = "${it}kcal"
+                binding.tvTotalCalories.text = totalCalories
+            }
+        }
+        vm.avgSpeedD.observe(viewLifecycleOwner) {
+            it?.let {
+                val avgSpeed = round(it * 10f) / 10f
+                val avgSpeedString = "${avgSpeed}km/h"
+                binding.tvAverageSpeed.text = avgSpeedString
+            }
+        }
+    }
+
+    private fun setToolbar() {
+        binding.tvTitle?.text = "LET'S GO, ${vm.getName().toUpperCase()}"
+    }
+
+    private fun setupBarChart() {
+        binding.barChart.xAxis.apply {
+            position = BOTTOM
+            setDrawLabels(false)
+            axisLineColor = Color.BLACK
+            textColor = Color.BLACK
+            setDrawGridLines(false)
+        }
+        binding.barChart.axisLeft.apply {
+            axisLineColor = Color.BLACK
+            textColor = Color.BLACK
+            setDrawGridLines(false)
+        }
+        binding.barChart.axisRight.apply {
+            axisLineColor = Color.BLACK
+            textColor = Color.BLACK
+            setDrawGridLines(false)
+        }
+        binding.barChart.apply {
+            description.text = "Avg Speed Over Time"
+            legend.isEnabled = false
+        }
     }
 
     override fun onDestroy() {
